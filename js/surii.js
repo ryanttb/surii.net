e94a7702823d1512c81bbfb34884c757ba370dbe
Ryan <ryan@trippingthebits.com>
2012-08-05 18:12:47 -0400

    don't auto scroll if not on 3ds

--- js/surii.js
+++ js/surii.js
@@ -1,8 +1,10 @@
 ﻿$(function () {
-  var $body = $("body");
-  if ($body.hasClass("skinny") || $body.hasClass("skinnyscroll")) {
-    window.scrollTo(0, 218);
-  } else if ($body.hasClass("tbone") || $body.hasClass("tbonescroll")) {
-    window.scrollTo(39, 218);
+  if ( navigator.userAgent.indexOf( "3DS" ) >= 0 ) {
+    var $body = $("body");
+    if ($body.hasClass("skinny") || $body.hasClass("skinnyscroll")) {
+      window.scrollTo(0, 218);
+    } else if ($body.hasClass("tbone") || $body.hasClass("tbonescroll")) {
+      window.scrollTo(39, 218);
+    }
   }
 });
