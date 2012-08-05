23f41b465e27ac2672a958ef2aa82e56ee38ab7b
Ryan <ryan@trippingthebits.com>
2012-08-05 18:14:35 -0400

    use main surii css & js instead of one for hack

--- hack/js/script.js
+++
@@ -1,8 +0,0 @@
-﻿$(function () {
-  var $body = $("body");
-  if ($body.hasClass("skinny") || $body.hasClass("skinnyscroll")) {
-    window.scrollTo(0, 218);
-  } else if ($body.hasClass("tbone") || $body.hasClass("tbonescroll")) {
-    window.scrollTo(39, 218);
-  }
-});
