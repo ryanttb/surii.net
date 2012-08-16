81e6860ab5d55434d2a62cad43707bde5b1ba0d4
Ryan <ryan@trippingthebits.com>
2012-08-15 23:46:23 -0400

    item info and a hacks section

--- js/index.js
+++ js/index.js
@@ -1,24 +1,63 @@
 ﻿$(function () {
-  var appsSurii = [ "Settings", "Tiny Cartridge" ],
+  function makeItem( name, item ) {
+    return '<li><a href="' + item.url + '" title="' + name + '"><img alt="" src="' + name + '/surii-icon.png" /><div class="app-info" style="display: none;"><h2>' + name + '</h2>' + '<span>' + (item.desc || '') + '</span>' + '</div></a></span></li>';
+  }
+
+  var appsSurii = {
+        "Settings": {
+          url: "Settings/",
+          desc: "surii.net settings"
+        },
+        "Tiny Cartridge": {
+          url: "Tiny Cartridge/",
+          desc: "3DS news"
+        }
+      },
       appsExternal = {
-        "OkiwiDS": "http://okiwids.co.cc/3DS/"
+        "OkiwiDS": {
+          url: "http://okiwids.co.cc/3DS/",
+          desc: "web browsing, games, chat"
+        }
+      },
+      appsHacks = {
+        "ryanttb": {
+          url: "hack/",
+          desc: "3DS browser tricks"
+        }
       },
       appItems = "",
       i = 0,
-      appEx;
+      appName,
+      selected = null;
 
-  for ( ; i < appsSurii.length; i++ ) {
-    appItems += '<li><a href="' + appsSurii[ i ] + '/" title="' + appsSurii[ i ] + '"><img alt="" src="' + appsSurii[ i ] + '/surii-icon.png" /></a></li>';
+  for ( appName in appsSurii ) {
+    appItems += makeItem( appName, appsSurii[ appName ] );
   }
 
   $("#appsSurii").html( appItems );
 
   appItems = "";
-  for ( appEx in appsExternal ) {
-    appItems += '<li><a href="' + appsExternal[ appEx ] + '" title="' + appEx + '"><img alt="" src="' + appEx + '/surii-icon.png" /></a></li>';
+  for ( appName in appsExternal ) {
+    appItems += makeItem( appName, appsExternal[ appName ] );
   }
 
   $("#appsExternal").html( appItems );
+
+  appItems = "";
+  for ( appName in appsHacks ) {
+    appItems += makeItem( appName, appsHacks[ appName ] );
+  }
+
+  $("#appsHacks").html( appItems );
+
+  $( "a" ).click( function( ) {
+    if ( this !== selected ) {
+      $( selected ).find( ".app-info" ).hide( );
+      $( this ).focus( ).find( ".app-info" ).show( );
+      selected = this;
+      return false;
+    }
+  } );
 
   if ( !surii.friendCode ) { 
     alert( "You have not entered your\n3DS Friend Code!\nPlease open Settings.\n\nSome games & apps require\na Friend Code to save data\nor interact with other users." );
